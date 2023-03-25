a51c92c9c5dfaf06d9a0aa4e7f3a457c61e9ccde
ChrisNdungu-dev <chrisnd.dev@gmail.com>
2023-03-25 13:42:50 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,63 @@
+// THEME
+
+// CALCULATOR
+
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".container");
+const display = calculator.querySelector(".display-label");
+const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+
+const backspace = calculator.querySelector(".delete-key");
+
+keys.addEventListener("click", (event) => {
+	if (!event.target.closest("button")) return;
+
+	const key = event.target;
+	const keyValue = key.textContent;
+	const displayValue = display.textContent;
+	const { type } = key.dataset;
+	const { previousKeyType } = calculator.dataset;
+	calculator.dataset.previousKeyType = type;
+
+	if (type === "number") {
+		if (displayValue === "0" || previousKeyType === "operator") {
+			display.textContent = keyValue;
+		} else {
+			display.textContent = displayValue + keyValue;
+		}
+	} else if (type === "operator") {
+		operatorKeys.forEach((el) => {
+			el.dataset.state = "";
+		});
+		key.dataset.state = "selected";
+
+		calculator.dataset.firstNumber = displayValue;
+		calculator.dataset.operator = key.dataset.key;
+	} else if (type === "equal") {
+		const firstNumber = calculator.dataset.firstNumber;
+		const operator = calculator.dataset.operator;
+		const secondNumber = displayValue;
+		display.textContent = calculate(firstNumber, operator, secondNumber);
+	} else if (type === "clear") {
+		display.textContent = "0";
+		delete calculator.dataset.firstNumber;
+		delete calculator.dataset.operator;
+	} else if (type === "delete") {
+		backspace.onclick = deleteSingle();
+	}
+});
+
+function deleteSingle() {
+	backspaceValue = display.textContent;
+	display.textContent = backspaceValue.substr(0, backspaceValue.length - 1);
+}
+
+function calculate(firstNumber, operator, secondNumber) {
+	firstNumber = parseInt(firstNumber);
+	secondNumber = parseInt(secondNumber);
+
+	if (operator === "plus") return firstNumber + secondNumber;
+	if (operator === "minus") return firstNumber - secondNumber;
+	if (operator === "times") return firstNumber * secondNumber;
+	if (operator === "divide") return firstNumber / secondNumber;
+}
